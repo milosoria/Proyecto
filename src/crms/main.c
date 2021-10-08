15a48b476c81d30d045a04604dfb5e9fc45e0f6f
milosoria <camilo.soria@uc.cl>
2021-10-08 11:03:19 -0300

    main file created

---
+++ src/crms/main.c
@@ -0,0 +1,13 @@
+#include "../api/crms_API.h"
+#include <stdlib.h>
+
+int main( int argc, char**argv){
+    if (argc != 2) {
+        printf("The execution needs to have the path to the virtual memory to run\n");
+        exit(0);
+    }
+
+    char * memory_path  = argv[1];
+    cr_mount(memory_path);
+    cr_ls_processes();
+}
