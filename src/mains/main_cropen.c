547b4f89efad03dfcb7f4461dd1d548499986f05
milosoria <camilo.soria@uc.cl>
2021-10-27 12:12:48 -0300

    run.sh added

---
+++ src/mains/main_cropen.c
@@ -0,0 +1,12 @@
+
+#include "../api/crms_API.h"
+#include <stdlib.h>
+#include <unistd.h>
+//#include <byteswap.h>
+
+int main( int argc, char**argv){
+    // FILE OPEN - w
+    printf("Ahora, probaremos la función CR_OPEN, modo 'w'.\n");
+
+    printf("\t END MAIN\n");
+}
